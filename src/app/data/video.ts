15eda837015446d75b705e9656eaaf7d55273258
gersonbreno <gersonfagundes2016@gmail.com>
2024-04-08 16:22:13 -0300

    versao 2

---
+++ src/app/data/video.ts
@@ -0,0 +1,52 @@
+export type Video = {
+    videoURL: string;
+    imageURL: string;
+    description: string;
+    title: string;
+}
+
+const videos: Video[] = [
+    {
+        videoURL: "video/abertura.mp4",
+        imageURL: "image/boku no kokoro.jpeg",
+        description: "Baku no Kokoro no Jabai Yatsu 2ª Temporada OP/Opening Song Phool",
+        title: "Boku no Kokoro abertura 2 Tempradas"
+    },
+    {
+        // video 2
+        videoURL: "video/Anime mood AMV.mp4",
+        imageURL: "image/AnimeMOOD.jpeg",
+        description: " videos de Anime com a musica mood",
+        title: "Anime mood AMV"
+    },
+    {   //video 3
+        videoURL: "video/Oregairu「AMV」Cadmium - No Friends.mp4",
+        imageURL: "image/pra video do oregaru.jpg",
+        description: "musicas no anime OregairuCadmium - No Friends",
+        title: "Oregairu「AMV」Cadmium - No Friends"
+    }
+    ,
+    {  //video 4
+        videoURL: "video/Rap do Hikigaya 『 Oregairu 』 _ PRESO EM MINHA SOLIDÃO _ AniRap e @MTD  (Prod. Hunter).mp4",
+        imageURL: "image/rap do hikigay.jpeg",
+        description: "rap do anime  Hikigaya  Oregairu ",
+        title: "Rap do Hikigaya 『 Oregairu 』 _ PRESO EM MINHA SOLIDÃO _ AniRap",
+    }
+    ,
+    {  //video 5
+        videoURL: "video/Rap do Oreki Houtarou『 Hyouka 』 _ Cansaço Mental _ Feat_ @Gabriza _ AniRap (@HunterMsc).mp4",
+        imageURL: "image/Rap do Oreki Houtarou『 Hyouka 』.jpeg",
+        description: "Rap do Oreki Houtaroul",
+        title: "Rap do Oreki Houtarou『 Hyouka 』 _ Cansaço Mental"
+    }
+    ,
+    {  //video 6
+        videoURL: "video/Rap do Ishida Shoya 『 Koe no Katachi 』 _Me Desculpa_ AniRap.mp4",
+        imageURL: "image/rap.jpg",
+        description: "Rap do Ishida Shoya ",
+        title: "Rap do Ishida Shoya 『 Koe no Katachi 』 _Me Desculpa_ AniRap"
+    }
+]
+
+
+export default videos;
